b174a233b4a96c7e71f731093067f9a4c498568e
Neelansh Keshari <lolster.the.loler@gmail.com>
2025-11-30 15:16:32 -0500

    Portal Part 1

--- Source/GAM415FirstPerson/GAM415FirstPersonCharacter.h
+++ Source/GAM415FirstPerson/GAM415FirstPersonCharacter.h
@@ -66,6 +66,7 @@ public:
 	USkeletalMeshComponent* GetMesh1P() const { return Mesh1P; }
 	/** Returns FirstPersonCameraComponent subobject **/
 	UCameraComponent* GetFirstPersonCameraComponent() const { return FirstPersonCameraComponent; }
-
+	UPROPERTY(EditAnywhere)
+		bool isTeleporting;
 };
 
